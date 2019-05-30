15a105cfe0706518c1490a0cf05521e7abf17259
saurabh.saraswat <saurabh.saraswat@CPX-J6CAOY67IK8.dir.svc.accenture.com>
2019-05-29 23:25:42 -0400

    j

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ provider "aws" {
 resource "aws_instance" "ec2_tf_sample_saurabh" {
 	ami = "ami-08a9b721ecc5b0a53"
 	instance_type = "t2.micro"
-	depends_on = ["aws_s3_bucket.s3_bucket_sample_saurabh"]
 }
 
 resource "aws_eip" "eip_tf_sample_saurabh" {
